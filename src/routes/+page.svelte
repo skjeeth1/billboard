8cf742998fde9aacb640beb9523978c97b36d589
Abhin Raj <abinr5501@gmail.com>
2026-05-30 13:32:28 +0530

    Move card border effect to another file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { fly } from 'svelte/transition';
 	import { onMount } from 'svelte';
-
+	import ElectroBorderFilter from './components/ElectroBorderFilter.svelte'
 	let animate = $state(false);
 
 	// Triggers the {#if} block to mount elements after the page loads
@@ -58,98 +58,7 @@
 </svelte:head>
 
 <div class="page-wrapper">
-	<svg class="svg-container">
-		<defs>
-			<filter
-				id="turbulent-displace"
-				colorInterpolationFilters="sRGB"
-				x="-20%"
-				y="-20%"
-				width="140%"
-				height="140%"
-			>
-				<feTurbulence
-					type="turbulence"
-					baseFrequency="0.02"
-					numOctaves="10"
-					result="noise1"
-					seed="1"
-				/>
-				<feOffset in="noise1" dx="0" dy="0" result="offsetNoise1">
-					<animate
-						attributeName="dy"
-						values="700; 0"
-						dur="6s"
-						repeatCount="indefinite"
-						calcMode="linear"
-					/>
-				</feOffset>
-
-				<feTurbulence
-					type="turbulence"
-					baseFrequency="0.02"
-					numOctaves="10"
-					result="noise2"
-					seed="1"
-				/>
-				<feOffset in="noise2" dx="0" dy="0" result="offsetNoise2">
-					<animate
-						attributeName="dy"
-						values="0; -700"
-						dur="6s"
-						repeatCount="indefinite"
-						calcMode="linear"
-					/>
-				</feOffset>
-
-				<feTurbulence
-					type="turbulence"
-					baseFrequency="0.02"
-					numOctaves="10"
-					result="noise1"
-					seed="2"
-				/>
-				<feOffset in="noise1" dx="0" dy="0" result="offsetNoise3">
-					<animate
-						attributeName="dx"
-						values="490; 0"
-						dur="6s"
-						repeatCount="indefinite"
-						calcMode="linear"
-					/>
-				</feOffset>
-
-				<feTurbulence
-					type="turbulence"
-					baseFrequency="0.02"
-					numOctaves="10"
-					result="noise2"
-					seed="2"
-				/>
-				<feOffset in="noise2" dx="0" dy="0" result="offsetNoise4">
-					<animate
-						attributeName="dx"
-						values="0; -490"
-						dur="6s"
-						repeatCount="indefinite"
-						calcMode="linear"
-					/>
-				</feOffset>
-
-				<feComposite in="offsetNoise1" in2="offsetNoise2" result="part1" />
-				<feComposite in="offsetNoise3" in2="offsetNoise4" result="part2" />
-				<feBlend in="part1" in2="part2" mode="color-dodge" result="combinedNoise" />
-
-				<feDisplacementMap
-					in="SourceGraphic"
-					in2="combinedNoise"
-					scale="30"
-					xChannelSelector="R"
-					yChannelSelector="B"
-				/>
-			</filter>
-		</defs>
-	</svg>
+	<ElectroBorderFilter/>
 
 	<section class="hero-section">
 		{#if animate}
@@ -210,13 +119,6 @@
 		margin: 0;
 		padding: 0;
 		overflow-x: hidden;
-	}
-
-	.svg-container {
-		position: absolute;
-		width: 0;
-		height: 0;
-		pointer-events: none;
 	}
 
 	.hero-section {
